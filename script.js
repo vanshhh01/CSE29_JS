46e9f2f933922ff01cfa5b5a7a21fe3af38cf193
vanshhh01 <vanshchauhan047@gmail.com>
2026-02-25 14:05:25 +0530

    js data types

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Hello World");
+var num = 10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+const sym1=Symbol(4)
+console.log(sym1);
+const sym2=Symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
