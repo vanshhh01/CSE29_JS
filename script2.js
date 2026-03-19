9fd2a68f3b8c869813b376e6a1e765b3e052ae13
vanshhh01 <vanshchauhan047@gmail.com>
2026-03-19 14:25:27 +0530

    DOM

---
+++ script2.js
@@ -0,0 +1,28 @@
+//const x=document.getElementById("p1");
+
+//console.log(x);
+//x.innerHTML="<b>Vanshhh</b>"
+
+function onClick(){
+    alert("onclick Event Launched");
+  }
+  
+  function onMouseOver(){
+    alert("onmouseover Event Launched");
+  }
+  
+  function onMouseOut(){
+    alert("onmouseout Event Launched");
+  }
+  
+  function onMouseDown(){
+    alert("onmousedown Event Launched");
+  }
+  
+  function onMouseUp(){
+    alert("onmouseup Event Launched");
+  }
+  
+  function onMouseMove(){
+    alert("onmousemove Event Launched");
+  }
